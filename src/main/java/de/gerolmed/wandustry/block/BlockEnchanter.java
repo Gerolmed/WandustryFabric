fc97e473e0cf9f37aa39c5090283a6061601aa8a
Gerolmed <werwolf420@yahoo.de>
2019-04-29 08:29:01 +0200

    Filled in osme of the enchanter code

--- src/main/java/de/gerolmed/wandustry/block/BlockEnchanter.java
+++ src/main/java/de/gerolmed/wandustry/block/BlockEnchanter.java
@@ -5,10 +5,15 @@ import de.gerolmed.wandustry.block.entity.EnchanterBlockEntity;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
+@SuppressWarnings("deprecation")
 public class BlockEnchanter extends BasicBlock implements BlockEntityProvider {
     public BlockEnchanter() {
         super("block_enchanter",
@@ -34,5 +39,16 @@ public class BlockEnchanter extends BasicBlock implements BlockEntityProvider {
     @Override
     public BlockEntity createBlockEntity(BlockView blockView) {
         return new EnchanterBlockEntity();
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public boolean activate(BlockState blockState_1, World world_1, BlockPos blockPos_1, PlayerEntity playerEntity_1, Hand hand_1, BlockHitResult blockHitResult_1) {
+
+        EnchanterBlockEntity enchanterBlockEntity = (EnchanterBlockEntity) world_1.getBlockEntity(blockPos_1);
+        enchanterBlockEntity.setItemStack(playerEntity_1.getMainHandStack());
+        System.out.println("Setting finished!");
+
+        return super.activate(blockState_1, world_1, blockPos_1, playerEntity_1, hand_1, blockHitResult_1);
     }
 }
--- src/main/java/de/gerolmed/wandustry/block/entity/EnchanterBlockEntity.java
+++ src/main/java/de/gerolmed/wandustry/block/entity/EnchanterBlockEntity.java
@@ -1,10 +1,43 @@
 package de.gerolmed.wandustry.block.entity;
 
 import de.gerolmed.wandustry.BlockEntities;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 
 public class EnchanterBlockEntity extends BasicBlockEntity {
+
+    private ItemStack itemStack;
+
     public EnchanterBlockEntity() {
         super(BlockEntities.ENCHANTER);
     }
 
+    @Override
+    public CompoundTag toTag(CompoundTag compoundTag_1) {
+
+        if(itemStack != null) {
+            compoundTag_1.put("item", itemStack.toTag(new CompoundTag()));
+        }
+
+        return super.toTag(compoundTag_1);
+    }
+
+    @Override
+    public void fromTag(CompoundTag compoundTag_1) {
+
+        if(compoundTag_1.containsKey("item")) {
+            this.itemStack = ItemStack.fromTag(compoundTag_1.getCompound("item"));
+        }
+
+        super.fromTag(compoundTag_1);
+    }
+
+    public void setItemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+        System.out.println("Setting item");
+    }
+
+    public ItemStack getItemStack() {
+        return itemStack;
+    }
 }
